d64680370a618b216307c52ae743aeb876a4ba99
codelifee <suminjung09@gmail.com>
2021-01-29 12:02:46 +0900

    fix: change package of spring boot application for component scan edited-by: sumin

---
+++ src/main/java/com/shoppingmall/ShoppingMallApplication.java
@@ -0,0 +1,19 @@
+package com.shoppingmall;
+
+
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MappedTypes(com.shoppingmall.model.Users.class)
+@MapperScan("com.shoppingmall.mapper")
+@SpringBootApplication
+public class ShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallApplication.class, args);
+	}
+
+}
+ 
