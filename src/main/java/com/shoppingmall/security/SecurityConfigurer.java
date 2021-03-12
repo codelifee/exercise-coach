b5cafe862b44a274c7ae4a2ba684627e62e67a6f
codelifee <suminjung09@gmail.com>
2021-03-12 16:59:27 +0900

    feat: add spring sequrity added-by: sumin

---
+++ src/main/java/com/shoppingmall/security/SecurityConfigurer.java
@@ -0,0 +1,48 @@
+package com.shoppingmall.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails sumin = User.builder()
+			.username("sumin")
+			.password("password")
+			.roles("user") //ROLE_USER
+			.build();
+		
+		return new InMemoryUserDetailsManager(
+					sumin
+				);
+		
+	}
+	
+	 
+	
+}
