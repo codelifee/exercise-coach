3c021f4f6f83fb538642fa29a32de05cdda8af89
sujin113 <rlatnwls651@gmail.com>
2021-03-19 10:48:11 +0900

    fix: payment

--- src/main/java/com/shoppingmall/mapper/PaymentMapper.java
+++ src/main/java/com/shoppingmall/mapper/PaymentMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -20,9 +19,8 @@ public interface PaymentMapper {
 	@Select("select * from payment where merchant_uid=#{merchant_uid}")
 	Payment getPayment(@Param("merchant_uid")String merchant_uid);
 	
-	@Insert("INSERT INTO payment VALUES(#{payment.merchant_uid},#{payment.user_sequence_id},#{payment.product_name}, "
-			+ "#{payment.amount}, #{payment.user_name}, #{payment.user_phone}, #{payment.user_id}, now())")
-	@Options(useGeneratedKeys = true, keyProperty = "answer_id")
+	@Insert("INSERT INTO payment VALUES(#{payment.merchant_uid},#{payment.product_name}, "
+			+ "#{payment.amount}, #{payment.user_name}, now())")
 	int insertPayment(@Param("payment") Payment payment);
 
 	@Delete("DELETE FROM payment WHERE merchant_uid=#{merchant_uid}")
--- src/main/java/com/shoppingmall/model/Payment.java
+++ src/main/java/com/shoppingmall/model/Payment.java
@@ -3,12 +3,9 @@ package com.shoppingmall.model;
 public class Payment {
 
 	private String merchant_uid;
-	private int user_sequence_id;
 	private String product_name;
 	private String amount;
 	private String user_name;
-	private String user_phone;
-	private String user_id;
 	private String date_of_payment;
 	
 	public String getMerchant_uid() {
@@ -16,12 +13,6 @@ public class Payment {
 	}
 	public void setMerchant_uid(String merchant_uid) {
 		this.merchant_uid = merchant_uid;
-	}
-	public int getUser_sequence_id() {
-		return user_sequence_id;
-	}
-	public void setUser_sequence_id(int user_sequence_id) {
-		this.user_sequence_id = user_sequence_id;
 	}
 	public String getProduct_name() {
 		return product_name;
@@ -34,18 +25,6 @@ public class Payment {
 	}
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
-	}
-	public String getUser_phone() {
-		return user_phone;
-	}
-	public void setUser_phone(String user_phone) {
-		this.user_phone = user_phone;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
 	}
 	public String getDate_of_payment() {
 		return date_of_payment;
